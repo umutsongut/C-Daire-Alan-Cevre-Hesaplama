91e899c9d936ea65d1ab2c19d60d97e8148b14fe
Umut Söngüt <1umut.songut@gmail.com>
2022-12-11 18:07:13 +0300

    Add project files.

---
+++ DoubleDegiskenAlistirma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleDegiskenAlistirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double yaricap,alan,cevre,pi;
+
+            pi = 3.14;
+
+            Console.Write("Yarıçap Uzunluğunu Girin: ");
+            yaricap = Convert.ToDouble(Console.ReadLine());
+
+            cevre = pi * yaricap * 2;
+            Console.WriteLine("Çevre:" + cevre);
+
+            alan = yaricap * yaricap * pi;
+            Console.Write("Alan:"+alan);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
